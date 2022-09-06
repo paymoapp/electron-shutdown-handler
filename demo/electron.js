672f6874f48dd3d938ab028feb0c6287267bac77
Fándly Gergő <gergo@paymoapp.com>
2022-09-06 14:39:18 +0300

    imp(demo): Better demo

--- demo/electron.js
+++ demo/electron.js
@@ -4,17 +4,50 @@ const ShutdownHandler = require('../dist').default;
 app.whenReady().then(() => {
 	const win = new BrowserWindow({
 		width: 600,
-		height: 300
+		height: 300,
+		title: 'Electron-shutdown-handler demo'
 	});
 
 	win.loadFile('index.html');
-	console.log('PID = ', process.pid);
+
+	console.log('--- Electron-shutdown-handler demo ---');
+
+	console.log('PID =', process.pid);
+	console.log('You can use rmlogotest.exe <PID> to "simulate" a shutdown');
+	console.log(
+		'You can close this app by executing Ctrl+C in the terminal or shutting down your system'
+	);
+
+	let allowQuit = false;
+
+	app.on('before-quit', e => {
+		if (!allowQuit) {
+			e.preventDefault();
+		}
+	});
+
+	win.on('close', e => {
+		if (!allowQuit) {
+			e.preventDefault();
+		}
+	});
 
 	ShutdownHandler.setWindowHandle(win.getNativeWindowHandle());
+	ShutdownHandler.blockShutdown('Yayy! It works! Shutdown in 10 seconds');
 
 	ShutdownHandler.on('shutdown', () => {
 		console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!');
 		console.log('! System is shutting down !');
 		console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+
+		console.log();
+		console.log('Setting a 10 second timer');
+
+		setTimeout(() => {
+			console.log('Shutting down NOW');
+			ShutdownHandler.releaseShutdown();
+			allowQuit = true;
+			app.quit();
+		}, 10_000);
 	});
 });

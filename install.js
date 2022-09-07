fa42a8d9522c148c8fe0687d0ba4a4c499ac067d
Fándly Gergő <gergo@paymoapp.com>
2022-09-07 18:41:47 +0300

    ci: Only build native module on windows

---
+++ install.js
@@ -0,0 +1,8 @@
+const { spawnSync } = require('child_process');
+
+if (process.platform == 'win32') {
+	console.log('Building addon');
+	spawnSync('npm', ['run', 'install:win32'], {
+		stdio: 'inherit'
+	});
+}
